5b7ec8be843ae2ce0ebd6dddaf338ba7f703485f
Gal Egozi <geegozi@gmail.com>
2021-02-04 00:02:07 -0600

    Add project files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    public Text scoreText;
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horiz, 0, vert);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            scoreText.text = "Score: " + count;
+        }
+    }
+}
