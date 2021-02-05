71b55c81db580fa53bb0773d80b604363d630060
Gal Egozi <geegozi@gmail.com>
2021-02-04 20:10:39 -0600

    Adding the controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,12 +18,23 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float horiz = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        float moveHorizontal = CrossPlatformInputManager.VirtualAxisReference("Horizontal").GetValue;
+        float moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horiz, 0, vert);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        //float moveHorizontal = Input.acceleration.x;
+        //float moveVertical = Input.acceleration.y;
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //GetComponent<Rigidbody>().AddForce(movement * speed * 2);
+
+        //float horiz = Input.GetAxis("Horizontal");
+        //float vert = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(horiz, 0, vert);
+
+        //GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
     }
 
     // Update is called once per frame
